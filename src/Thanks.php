e1949889b29aae694026fc99b89c3fd82cbd2a3a
Jáchym Toušek <enumag@gmail.com>
2020-10-29 13:41:59 +0100

    Update to composer-plugin-api v2

--- src/Thanks.php
+++ src/Thanks.php
@@ -11,4 +11,12 @@ class Thanks implements PluginInterface
     public function activate(Composer $composer, IOInterface $io): void
     {
     }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+    }
 }
